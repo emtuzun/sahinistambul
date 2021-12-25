89d9841303bd03b7185811a45d0d056ce689b169
emtuzun <emtuzun@gmail.com>
2021-12-25 19:40:47 +0300

    delete frontend

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import router from './router/router.js'
-import path from 'path';
+
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 dotenv.config()
 app.use('/api/articles', router)
-app.use(express.static('client/static'))
 
-app.get("/*", (req, res) =>{
-    res.sendFile(path.resolve("client", "index.html"))
-})
 
 app.listen(process.env.PORT, () => {
     mongoose.connect(process.env.MONGO_URI, {
